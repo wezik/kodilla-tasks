369fc2428a7d8775ee75ae0b8ad2fadd7c96a699
Dominik Kondrot <dominik.kondrot@gmail.com>
2020-12-10 16:16:57 +0100

    23.2 zadanie

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -22,7 +22,7 @@ public class CoreConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
