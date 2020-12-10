229da12daeb88c1c0df742f763fed0ee6891699c
Dominik Kondrot <dominik.kondrot@gmail.com>
2020-12-10 17:05:32 +0100

    23.3

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService emailService;
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @Test
+    void shouldSendEmail() {
+        //Given
+        Mail mail = new Mail("test@test.com","Test","Test message");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        //When
+        emailService.send(mail);
+
+        //Then
+        verify(mailSender,times(1)).send(mailMessage);
+    }
+}
