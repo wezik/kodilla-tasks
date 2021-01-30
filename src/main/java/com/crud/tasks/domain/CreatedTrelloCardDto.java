ce22a68daabf5e77bcdef5e6b19d924e71dcbad3
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-01-30 19:32:02 +0100

    29.3

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCardDto.java
@@ -23,4 +23,10 @@ public class CreatedTrelloCardDto {
 
     @JsonProperty("badges")
     private Badges badges;
+
+    public CreatedTrelloCardDto(String id, String name, String shortUrl) {
+        this.id = id;
+        this.name = name;
+        this.shortUrl = shortUrl;
+    }
 }
